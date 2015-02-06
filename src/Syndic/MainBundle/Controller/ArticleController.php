ab1ca8b74fac15f3445a27559ddb1d008426cd8d
Arthurhall <arthur.hall@orange.fr>
2015-02-06 20:34:24 +0100

    Deactivated Article CRUD actions

--- src/Syndic/MainBundle/Controller/ArticleController.php
+++ src/Syndic/MainBundle/Controller/ArticleController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Syndic\MainBundle\Entity\Article;
 use Syndic\MainBundle\Form\ArticleType;
 
@@ -14,91 +15,13 @@ use Syndic\MainBundle\Form\ArticleType;
  * Article controller.
  *
  * @Route("/article")
+ * 
+ * Pour l'instant seul l'action "show" est utilisée
+ * les autres actions ont été générées
+ * les annotations sont désactivées, il manque le "@" !
  */
 class ArticleController extends Controller
 {
-
-    /**
-     * Lists all Article entities.
-     *
-     * @Route("/", name="article")
-     * @Method("GET")
-     * @Template()
-     */
-    public function indexAction()
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $entities = $em->getRepository('SyndicMainBundle:Article')->findAll();
-
-        return array(
-            'entities' => $entities,
-        );
-    }
-    /**
-     * Creates a new Article entity.
-     *
-     * @Route("/", name="article_create")
-     * @Method("POST")
-     * @Template("SyndicMainBundle:Article:new.html.twig")
-     */
-    public function createAction(Request $request)
-    {
-        $entity = new Article();
-        $form = $this->createCreateForm($entity);
-        $form->handleRequest($request);
-
-        if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($entity);
-            $em->flush();
-
-            return $this->redirect($this->generateUrl('article_show', array('id' => $entity->getId())));
-        }
-
-        return array(
-            'entity' => $entity,
-            'form'   => $form->createView(),
-        );
-    }
-
-    /**
-     * Creates a form to create a Article entity.
-     *
-     * @param Article $entity The entity
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createCreateForm(Article $entity)
-    {
-        $form = $this->createForm(new ArticleType(), $entity, array(
-            'action' => $this->generateUrl('article_create'),
-            'method' => 'POST',
-        ));
-
-        $form->add('submit', 'submit', array('label' => 'Create'));
-
-        return $form;
-    }
-
-    /**
-     * Displays a form to create a new Article entity.
-     *
-     * @Route("/new", name="article_new")
-     * @Method("GET")
-     * @Template()
-     */
-    public function newAction()
-    {
-        $entity = new Article();
-        $form   = $this->createCreateForm($entity);
-
-        return array(
-            'entity' => $entity,
-            'form'   => $form->createView(),
-        );
-    }
-
     /**
      * Finds and displays a Article entity.
      *
@@ -122,20 +45,89 @@ class ArticleController extends Controller
             return $ca->getRedirectResponse($article->getCategory());
         }
 
-        $deleteForm = $this->createDeleteForm($article->getId());
-
         return array(
             'article'      => $article,
-            'delete_form' => $deleteForm->createView(),
+        );
+    }
+    
+    
+    /**
+     * Lists all Article entities.
+     *
+     * Route("/", name="article")
+     * Method("GET")
+     * Template()
+     * 
+     * Security("has_role('ROLE_ADMIN')")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('SyndicMainBundle:Article')->findAll();
+
+        return array(
+            'entities' => $entities,
+        );
+    }
+    
+    /**
+     * Creates a new Article entity.
+     *
+     * Route("/", name="article_create")
+     * Method("POST")
+     * Template("SyndicMainBundle:Article:new.html.twig")
+     * 
+     * Security("has_role('ROLE_ADMIN')")
+     */
+    public function createAction(Request $request)
+    {
+        $entity = new Article();
+        $form = $this->createCreateForm($entity);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('article_show', array('id' => $entity->getId())));
+        }
+
+        return array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        );
+    }
+
+    /**
+     * Displays a form to create a new Article entity.
+     *
+     * Route("/new", name="article_new")
+     * Method("GET")
+     * Template()
+     * 
+     * Security("has_role('ROLE_ADMIN')")
+     */
+    public function newAction()
+    {
+        $entity = new Article();
+        $form   = $this->createCreateForm($entity);
+
+        return array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
         );
     }
 
     /**
      * Displays a form to edit an existing Article entity.
      *
-     * @Route("/{id}/edit", name="article_edit")
-     * @Method("GET")
-     * @Template()
+     * Route("/{id}/edit", name="article_edit")
+     * Method("GET")
+     * Template()
+     * 
+     * Security("has_role('ROLE_ADMIN')")
      */
     public function editAction($id)
     {
@@ -158,29 +150,13 @@ class ArticleController extends Controller
     }
 
     /**
-    * Creates a form to edit a Article entity.
-    *
-    * @param Article $entity The entity
-    *
-    * @return \Symfony\Component\Form\Form The form
-    */
-    private function createEditForm(Article $entity)
-    {
-        $form = $this->createForm(new ArticleType(), $entity, array(
-            'action' => $this->generateUrl('article_update', array('id' => $entity->getId())),
-            'method' => 'PUT',
-        ));
-
-        $form->add('submit', 'submit', array('label' => 'Update'));
-
-        return $form;
-    }
-    /**
      * Edits an existing Article entity.
      *
-     * @Route("/{id}", name="article_update")
-     * @Method("PUT")
-     * @Template("SyndicMainBundle:Article:edit.html.twig")
+     * Route("/{id}", name="article_update")
+     * Method("PUT")
+     * Template("SyndicMainBundle:Article:edit.html.twig")
+     * 
+     * Security("has_role('ROLE_ADMIN')")
      */
     public function updateAction(Request $request, $id)
     {
@@ -208,11 +184,14 @@ class ArticleController extends Controller
             'delete_form' => $deleteForm->createView(),
         );
     }
+    
     /**
      * Deletes a Article entity.
      *
-     * @Route("/{id}", name="article_delete")
-     * @Method("DELETE")
+     * Route("/{id}", name="article_delete")
+     * Method("DELETE")
+     * 
+     * Security("has_role('ROLE_ADMIN')")
      */
     public function deleteAction(Request $request, $id)
     {
@@ -233,7 +212,27 @@ class ArticleController extends Controller
 
         return $this->redirect($this->generateUrl('article'));
     }
+    
+    
+    /**
+     * Creates a form to edit a Article entity.
+     *
+     * @param Article $entity The entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createEditForm(Article $entity)
+    {
+        $form = $this->createForm(new ArticleType(), $entity, array(
+            'action' => $this->generateUrl('article_update', array('id' => $entity->getId())),
+            'method' => 'PUT',
+        ));
 
+        $form->add('submit', 'submit', array('label' => 'Update'));
+
+        return $form;
+    }
+    
     /**
      * Creates a form to delete a Article entity by id.
      *
@@ -249,5 +248,24 @@ class ArticleController extends Controller
             ->add('submit', 'submit', array('label' => 'Delete'))
             ->getForm()
         ;
+    }
+    
+    /**
+     * Creates a form to create a Article entity.
+     *
+     * @param Article $entity The entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createCreateForm(Article $entity)
+    {
+        $form = $this->createForm(new ArticleType(), $entity, array(
+            'action' => $this->generateUrl('article_create'),
+            'method' => 'POST',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Create'));
+
+        return $form;
     }
 }
